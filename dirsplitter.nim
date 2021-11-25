ae387c549df96c86f9c8e0955622f24656c6102f
jinyus <starboymc4@gmail.com>
2021-11-25 13:30:34 -0500

    use argparse package and walkDirRec instead. Other  refacotring

--- dirsplitter.nim
+++ dirsplitter.nim
@@ -1,107 +1,110 @@
 import os
 import tables
 import strutils
-import parseopt
 import parseutils
 import strformat
+import confirm_op
+import argparse
 
-proc confirmOperation(desc:string)
-
-var p = initOptParser(commandLineParams(),shortNoVal = {'s'},longNoVal = @["show-cmd"])
-
-var dir : string = "."
-var max : BiggestFloat = 5.0
-var showCmd : bool = false
-var outPrefix : string = ""
-
-while true:
-  p.next()
-  case p.kind
-  of cmdEnd: break
-  of cmdShortOption, cmdLongOption:
-    case p.key:
-    of "dir","d":
-        dir = p.val
-    of "max","m":
-        discard parseBiggestFloat(p.val,max,0)
-    of "show-cmd","s":
-        showCmd = true
-    of "out-prefix","o":
-        outPrefix = p.val
-    else: discard
-  of cmdArgument:
-      continue
-
-if not outPrefix.isEmptyOrWhitespace:
-    outPrefix.add('.')
-
-confirmOperation("Splitting \"{dir}\" into {max}GB parts.".fmt)
-
-echo "Splitting Directory\n\n"
+proc splitDir(dir: string, maxFilesize: BiggestInt, prefix: string, show: bool)
+# proc reverseSplitDir(dir: string)
 
 const GBMultiple = 1024 * 1024 * 1024
-var tracker : Table[int, BiggestFloat] = {1: 0.toBiggestFloat}.toTable  
-var currentPart = 1
-var filesMoved = 0
-var failedOps = 0
-var maxFileSize :BiggestFloat = max * GBMultiple
 
-for kind, path in walkDir(dir):
-    if  kind != pcFile:
-        continue
+let p = argparse.newParser:
+    help("Split directories into a specified maximum size")
+    option("-d", "--dir", default = some("."), help = "Target directory")
+    option("-m", "--max", default = some("5.0"), help = "Max part size in GB")
+    option("-p", "--prefix", default = some(""),
+            help = "Prefix for output files of the tar command. -show-cmd must be specified. eg: myprefix.part1.tar")
+    flag("-s", "--show", help = "Show tar command to compress each directory")
+    run:
+        let dir = opts.dir.strip()
+        var max: BiggestFloat = 5.0
+        let result =  parseBiggestFloat(opts.max, max, 0)
+        if result == 0:
+            echo "Invalid number for max \"{opts.max}\"".fmt
+            quit(1)
 
-    var size: BiggestFloat
-    var decrementIfFailed = false
-    try:
-        size = getFileSize(path).toBiggestFloat
-        if tracker[currentPart] + size > maxFileSize and tracker[currentPart] > 0.0:
-            inc currentPart
-            decrementIfFailed = true
-        discard tracker.hasKeyOrPut(currentPart,0.toBiggestFloat)
-        tracker[currentPart] += size
-    except OSError:
-        echo "failed to get filesize : " & getCurrentExceptionMsg()
-        continue 
+        let show = opts.show
+        let outputPrefix = (if opts.prefix.isEmptyOrWhitespace:"" else:opts.prefix & ".")
 
-    var filename = extractFilename(path)
-    var partDir = joinPath(dir,"part" & intToStr(currentPart))
-    discard existsOrCreateDir(partDir)
+        confirmOperation(fmt "Splitting \"{dir}\" into {max}GB parts.")
 
-    try:
-        moveFile(path,joinPath(partDir,filename))
-        inc filesMoved
-    except OSError:
-        echo "failed to move file : " & getCurrentExceptionMsg()
-        inc failedOps
-        tracker[currentPart] = tracker[currentPart] - size
-        if decrementIfFailed:
-            dec currentPart
+        if not os.dirExists(dir):
+            echo "Directory {dir} doesn't exists."
+            quit(1)
 
-if filesMoved == 0:
-    currentPart = 0
+        splitDir(
+            dir,
+            maxFilesize = (max * GBMultiple).toBiggestInt,
+            outputPrefix,
+            show
+        )
 
-echo "Done"
-echo "Parts created: " & currentPart.intToStr
-echo "Files moved: " & filesMoved.intToStr
-echo "Failed Operations: " & failedOps.intToStr
+    command("reverse"):
+        help("opposite of the main function, moves all files in part folders to the root")
+        option("-d", "--dir", default = some("."), help = "Target directory")
+        run:
+            echo opts.dir
 
-if currentPart > 0 and showCmd:
-    if currentPart == 1:
-        echo fmt"""Tar Command : tar -cf "{outPrefix}part1.tar" "part1"; done"""
-    else:
-        echo fmt"""Tar Command : for n in {{1..{currentPart}}}; do tar -cf "{outPrefix}part$n.tar" "part$n"; done"""
+try:
+  p.run(os.commandLineParams())
+except ShortCircuit as e:
+  if e.flag == "argparse_help":
+    echo p.help
+    quit(1)
+except UsageError:
+  stderr.writeLine getCurrentExceptionMsg()
+  quit(1)
 
+proc splitDir(dir: string, maxFilesize: BiggestInt, prefix: string, show: bool)=
+    echo "\nSplitting Directory...\n\n"
 
-proc confirmOperation(desc:string)= 
-    echo desc
-    write(stdout, "continue? (y/n): ")
-    var answer =  readLine(stdin).strip.toLower
-    case answer:
-    of "y","yes":
-        return
-    of "n","no":
-        echo "Goodbye!"
-        quit(0)
-    else:
-        echo fmt"invalid answer : expected (y or n) got ({answer})" 
-        quit(1)
+    var tracker: Table[int, BiggestInt] = {1: 0.toBiggestInt}.toTable
+    var currentPart = 1
+    var filesMoved = 0
+    var failedOps = 0
+
+    for path in os.walkDirRec(dir):
+        let size = os.getFileSize(path)
+
+        #the filesize is added to the table so decrement is the move op fails
+        var decrementIfFailed = false 
+
+        try:
+            if tracker[currentPart] + size > maxFileSize and tracker[currentPart] > 0:
+                inc currentPart
+                decrementIfFailed = true
+            discard tracker.hasKeyOrPut(currentPart, 0)
+            tracker[currentPart] += size
+        except OSError:
+            echo "failed to get filesize : " & getCurrentExceptionMsg()
+            continue
+
+        var filename = os.extractFilename(path)
+        var partDir = os.joinPath(dir, fmt"part{currentPart}")
+        discard os.existsOrCreateDir(partDir)
+
+        try:
+            os.moveFile(path, joinPath(partDir, filename))
+            inc filesMoved
+        except OSError:
+            echo "failed to move file : \n" & getCurrentExceptionMsg()
+            inc failedOps
+            tracker[currentPart] -= size
+            if decrementIfFailed:
+                dec currentPart
+
+    if filesMoved == 0:
+        currentPart = 0
+
+    echo fmt"Parts created: {currentPart}"
+    echo fmt"Files moved: {filesMoved}"
+    echo fmt"Failed Operations: {failedOps}"
+
+    if currentPart > 0 and show:
+        if currentPart == 1:
+            echo fmt"""Tar Command : tar -cf "{prefix}part1.tar" "part1"; done"""
+        else:
+            echo fmt"""Tar Command : for n in {{1..{currentPart}}}; do tar -cf "{prefix}part$n.tar" "part$n"; done"""
